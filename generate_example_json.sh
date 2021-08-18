3b61879ac80468331f3144c5c633a47da0cbf605
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-18 08:26:42 +0100

    Machine generate example JSON

---
+++ generate_example_json.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir -p tmp_for_json_generate
+rm -r tmp_for_json_generate/* || true
+cp *.schema.json tmp_for_json_generate
+cd tmp_for_json_generate
+
+for i in `ls *.schema.json`
+do
+    echo "Editing $i ..."
+    sed -f ../../../replace_unknown_json_schema_types.sed < $i  > $i.tmp
+    mv $i.tmp $i
+    example_file_name=${i/.schema.json/.example.json}
+    echo $example_file_name
+    fake-schema $i > $example_file_name
+done
+
+fake-schema event.schema.json > event.example.json
+
+cd ..
+
+cp tmp_for_json_generate/*.example.json .
+rm -rf tmp_for_json_generate
---
+++ replace_unknown_json_schema_types.sed
@@ -0,0 +1,2 @@
+s/"type": "uuid"/"type": "string"/g
+s/"type": "date-time"/"type": "string"/g
