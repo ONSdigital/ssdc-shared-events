4c1c692b8f1453292aa0e557d28c798f36d1efe4
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-19 17:26:28 +0100

    Add formatting to regen script

--- regen.sh
+++ regen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd event_dictionary/v0.3_DRAFT/
+npx prettier --write ./*.json
 generate-schema-doc event.schema.json --config template_name=md dictionary.md
 ../../generate_example_json.sh
 cd ../..
